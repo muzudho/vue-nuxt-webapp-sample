eecdedf3e1b13bba920e7af88c74d497a90ebed3
muzudho <muzudho1@gmail.com>
2025-08-04 17:47:57 +0900

    Bug: NuxtとVuetify3の連携がまだできてない？

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col cols="11">
+                        Vuetify3と連携
+                    </v-col>
+                    <v-col cols="1">
+                        テスト１
+                    </v-col>
+                    <v-btn>あいうボタン</v-btn>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
+    <div>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+    </div>
 </template>
