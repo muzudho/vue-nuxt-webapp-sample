e1f470c6335a922fb0df1bf7dfc4a231885d7746
muzudho <muzudho1@gmail.com>
2025-08-08 20:15:32 +0900

    メイキング

--- pages/making/index.vue
+++ pages/making/index.vue
@@ -4,7 +4,7 @@
         アプリケーション製作の練習だぜ（＾▽＾）！<br/>
 
         <ul>
-            <li><router-link to="/making/page-layout">説明を始める前に、説明ページのレイアウトを決めておこうぜ！</router-link></li>
+            <li>📖 <router-link to="/making/page-layout">説明を始める前に、説明ページのレイアウトを決めておこうぜ！</router-link></li>
             <li>📖 <router-link to="/making/mount-practice">onMounted を使う練習だぜ！</router-link></li>
         </ul>
     </section>
