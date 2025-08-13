611e50e272412128d4e068ef31105d4095fa797d
muzudho <muzudho1@gmail.com>
2025-08-13 19:08:12 +0900

    戻るリンクをボタンに変更

--- src/pages/making/the-header.vue
+++ src/pages/making/the-header.vue
@@ -3,7 +3,10 @@
 
     <h2>メイキングの章だぜ！</h2>
     <section class="sec-2">
-        <router-link to="/making">この章の先頭に戻る</router-link>
+        <v-btn @click="goToMaking" v-tooltip="'下の節を閉じる'">❌</v-btn>
+        <!--
+            <router-link to="/making">この章の先頭に戻る</router-link>
+        -->
     </section>
 </template>
 
@@ -19,4 +22,13 @@
 
     import TheParentHeader from '../the-header.vue';
 
+
+    // ############
+    // # ルーチン #
+    // ############
+    const router = useRouter();
+
+    function goToMaking() : void {
+        router.push('/making');
+    }
 </script>
