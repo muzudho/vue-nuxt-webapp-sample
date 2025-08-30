6f1029aadf5a3076a9d2b4c4a106aebf3d2fc110
muzudho <muzudho1@gmail.com>
2025-08-31 08:45:41 +0900

    Talker をコンポーネント化

---
+++ src/composables/talk-scene.ts
@@ -0,0 +1,28 @@
+// ##############
+// # インポート #
+// ##############
+
+// ++++++++++++++
+// + 互換性対応 +
+// ++++++++++++++
+
+import type { CompatibleStyleValue }  from '../compatibles/compatible-style-value';
+
+
+// ################
+// # エクスポート #
+// ################
+
+/**
+ * 画像の下に付けるキャプションのスタイル
+ */
+export const illustration1FaceStyle = computed<
+    (isMobileMaybeOption: boolean | null) => CompatibleStyleValue
+>(()=>{
+    return (isMobileMaybeOption: boolean | null) => {
+        const isMobileMaybe: boolean = (isMobileMaybeOption == null) ? false : isMobileMaybeOption;
+        return {
+            width: isMobileMaybe ? '80px' : '96px',
+        } as CompatibleStyleValue;
+    };
+});
