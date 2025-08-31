8400835d7c41707579818dd42e45eee8dd2f849a
muzudho <muzudho1@gmail.com>
2025-08-31 19:29:38 +0900

    WIP: Talk 吹き出しのコンポーネント化準備

---
+++ src/components/TalkerBalloon.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="talk">
+        <div class="face-container">
+            <img
+                :src="props.src"
+                :alt="props.alt" />
+        </div>
+        <div class="baloon-tail"></div>
+        <div class="baloon">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+    // ####################################
+    // # このコンポーネントが受け取る引数 #
+    // ####################################
+
+    interface Props {
+        src: string,
+        alt: string,
+    }
+    const props = defineProps<Props>();
+
+</script>
+
+<style lang="css" scoped>
+</style>
