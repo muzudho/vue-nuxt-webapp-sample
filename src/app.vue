95d05bf2753264e24dc1d3d1688fbeaa1aeaef32
muzudho <muzudho1@gmail.com>
2025-09-09 19:48:59 +0900

    Tauri とコード合わせ

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,27 @@
 <template>
     <v-app>
         <v-main>
+
+            <!-- Nuxt + Vue ウェブアプリ用：
+                <v-container> は：
+                    スマホでのレイアウトの崩れを防止します。
+                    ただし、どんなに画面の横幅が広くても、コンテンツはセンタリングされてしまうという欠点はあります
+                <NuxtPage> は：
+                    この部分に、pages フォルダーの中にある Vue コンポーネント（.vue ファイル）がレンダリングされます。
+            -->
             <NuxtLayout>
-                <!--
-                    v-container は、レイアウトの崩れを防止します。
-                    どんなに画面の横幅が広くても、コンテンツはセンタリングされてしまうという欠点はあります
-                -->
                 <v-container>
-
-                    <!-- /pages フォルダーの中にある .vue ファイル がここに埋め込まれます。 -->
                     <NuxtPage />
-
                 </v-container>
             </NuxtLayout>
+
+            <!-- Tauri + Vue デスクトップアプリ用：
+                <router-view> は：
+                    Nuxt の <NuxtPage> に相当し、この部分にコンポーネントがレンダリングされます。
+
+            <router-view />
+            -->
+
         </v-main>
     </v-app>
 </template>
