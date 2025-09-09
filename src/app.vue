e4cc85c264e6802ae3e712378dacae47b078b675
muzudho <muzudho1@gmail.com>
2025-09-09 19:32:27 +0900

    <v-container>

--- src/app.vue
+++ src/app.vue
@@ -2,10 +2,16 @@
     <v-app>
         <v-main>
             <NuxtLayout>
+                <!--
+                    v-container は、レイアウトの崩れを防止します。
+                    どんなに画面の横幅が広くても、コンテンツはセンタリングされてしまうという欠点はあります
+                -->
+                <v-container>
 
-                <!-- /pages フォルダーの中にある .vue ファイル がここに埋め込まれます。 -->
-                <NuxtPage />
+                    <!-- /pages フォルダーの中にある .vue ファイル がここに埋め込まれます。 -->
+                    <NuxtPage />
 
+                </v-container>
             </NuxtLayout>
         </v-main>
     </v-app>
