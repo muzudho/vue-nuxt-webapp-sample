efc39a95eab445463a56f813473c852c493ae5bd
muzudho <muzudho1@gmail.com>
2025-09-15 06:27:14 +0900

    JSONファイル読込　＞　#public 対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
-    css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
+    alias: {
+        '#public': './public', // public/ フォルダをエイリアス
+    },
     build: {
         transpile: ["vuetify"],
     },
-    srcDir: 'src/', // srcフォルダをルートに設定
+    compatibilityDate: '2025-07-15',
+    css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
+    devtools: { enabled: true },
     dir: {  // マッピングか？
         assets: 'assets', // src/assets
         //components: 'components', // src/components
         //composables: 'composables', // src/composables
         pages: 'pages', // src/pages
         plugins: 'plugins', // src/plugins
-        //public: './public',
         public: '../public', // src から見て ../public
     },
+    runtimeConfig: {
+        public: {
+            baseUrl: process.env.VITE_BASE_URL || 'http://localhost:3000/', // デフォルト値として開発環境のURLを設定
+        },
+    },
+    srcDir: 'src/', // srcフォルダをルートに設定
 })
